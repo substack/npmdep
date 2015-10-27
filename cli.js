42b325da056f951b19da15f8be1455460e9ea3f1
Anders Riutta <git@andersriutta.com>
2015-10-27 13:55:35 -0700

    path.exists deprecated. now using fs.stat. npm registry looks for README.md, not README.markdown.

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var npmdep = require('npmdep');
+var npmdep = require('./index.js');
 var Hash = require('hashish');
 var Traverse = require('traverse');
 var Seq = require('seq');
